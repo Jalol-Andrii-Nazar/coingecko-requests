6bd2254464b7abb71f2f4fb1de516ea4a51e19c9
Nazar Kalinowski <nazark@tutanota.com>
2021-03-12 21:27:57 +0200

    Fix API_URL and add ping func

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-const API_URL: &'static str = "https://www.coingecko.com/api/documentations/v3";
+const API_URL: &'static str = "https://api.coingecko.com/api/v3";
 
 struct Client {}
 
 impl Client {
-    fn new(&self) -> Self {
+    fn new() -> Self {
         Self {}
+    }
+
+    async fn ping(&self) -> Result<String, reqwest::Error> {
+        reqwest::get(format!("{}/ping", API_URL))
+            .await?
+            .text()
+            .await
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("ping")
-        .await?
-        .text()
-        .await?;
-    println!("{:#?}", resp);
+    let client = Client::new();
+    let res = client.ping().await?;
+    println!("Ping result: {}", res);
     Ok(())
 }
