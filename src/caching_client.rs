3ab10657869eeb194f3549bb96b3163c62b22307
CODPUD <jalol.kham.2401@gmail.com>
2021-05-19 20:01:09 +0200

    trigger-fixed

--- src/caching_client.rs
+++ src/caching_client.rs
@@ -49,7 +49,7 @@ impl Client {
         sqlx::query("CREATE TABLE IF NOT EXISTS market_chart_range_total_volumes (parent_rowid INTEGER, timestamp INTEGER, value REAL, CONSTRAINT parent_fk FOREIGN KEY (parent_rowid) REFERENCES market_chart_range_meta (rowid))")
             .execute(&conn)
             .await?;
-        sqlx::query("CREATE TABLE IF NOT EXISTS triggers (trigger_id INTEGER PRIMARY KEY AUTOINCREMENT, coin TEXT, currency TEXT, old_price INTEGER, from_ INTEGER, to_ INTEGER)")
+        sqlx::query("CREATE TABLE IF NOT EXISTS triggers (trigger_id INTEGER PRIMARY KEY AUTOINCREMENT, coin TEXT, currency TEXT, old_price INTEGER, new_price INTEGER)")
             .execute(&conn)
             .await?;
 
@@ -301,13 +301,12 @@ impl Client {
         Ok(meta_rowid)
     }
 
-    pub async fn add_trigger(&mut self, coin: &str, currency: &str, old_price: u64, from: u64, to: u64) -> Result<(), Box<dyn std::error::Error>> {
-        sqlx::query("INSERT INTO triggers(coin, currency, old_price, from_, to_) VALUES (?, ?, ?, ?, ?)")
+    pub async fn add_trigger(&mut self, coin: &str, currency: &str, old_price: u64, value: u64) -> Result<(), Box<dyn std::error::Error>> {
+        sqlx::query("INSERT INTO triggers(coin, currency, old_price, new_price) VALUES (?, ?, ?, ?)")
             .bind(coin)
             .bind(currency)
             .bind(old_price as i64)
-            .bind(from as i64)
-            .bind(to as i64)
+            .bind(value as i64)
             .execute(&self.conn)
             .await?;
         Ok(())
